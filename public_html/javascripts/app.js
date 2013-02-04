4a84abf55ac48c8cc1f5a360fe99d4513b2da3e1
Edward Finkler <coj@funkatron.com>
2013-02-03 23:31:11 -0500

    update index.html to display microphp collection feed from Gimme Bar

--- public_html/javascripts/app.js
+++ public_html/javascripts/app.js
@@ -1,14 +1,12 @@
-function FirstController($scope) {
-	$scope.data = {
-		items: [
-			{
-				name: "Foo",
-				description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus nisl dolor, pulvinar id, pharetra a, egestas nec, ante. Duis scelerisque eleifend metus. Sed non odio id odio varius rutrum. Pellentesque congue commodo lacus. In semper pede lacinia felis. Morbi mollis molestie lorem. Morbi suscipit libero. Quisque ut erat sit amet elit aliquam nonummy. Donec tortor. Aliquam gravida ullamcorper pede. Praesent eros. Sed fringilla ligula sed odio pharetra imperdiet. Integer aliquet quam vitae nibh. Nam pretium, neque non congue vulputate, odio odio vehicula augue, sit amet gravida pede massa ac lectus. Curabitur a libero vitae dui sagittis aliquet. Ut suscipit. Curabitur accumsan sem a urna. Ut elit pede, vulputate sed, feugiat quis, congue sed, lacus."
-			},
-			{
-				name: "Bar",
-				description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus nisl dolor, pulvinar id, pharetra a, egestas nec, ante. Duis scelerisque eleifend metus. Sed non odio id odio varius rutrum. Pellentesque congue commodo lacus. In semper pede lacinia felis. Morbi mollis molestie lorem. Morbi suscipit libero. Quisque ut erat sit amet elit aliquam nonummy. Donec tortor. Aliquam gravida ullamcorper pede. Praesent eros. Sed fringilla ligula sed odio pharetra imperdiet. Integer aliquet quam vitae nibh. Nam pretium, neque non congue vulputate, odio odio vehicula augue, sit amet gravida pede massa ac lectus. Curabitur a libero vitae dui sagittis aliquet. Ut suscipit. Curabitur accumsan sem a urna. Ut elit pede, vulputate sed, feugiat quis, congue sed, lacus."
+function FirstController($scope, $http) {
+	var url = "https://gimmebar.com/api/v0/public/assets/funkatron/micro-php.json";
+	$http.jsonp(url, {
+			'params':{
+				'limit':50,
+				'jsonp_callback':'JSON_CALLBACK'
 			}
-		]
-	};
+		})
+		.success(function(data, status) {
+			$scope.data = data;
+		});
 }
